ee19aa8b8335d9dc98bd99b20e5bcd08b3f9f409
i3p9 <i3p9@users.noreply.github.com>
2023-07-23 00:09:32 +0600

    update base url for production

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,7 +1,7 @@
 const prod = {
     url: {
         API_URL: 'https://transcript-search.onrender.com',
-        BASE_URL: 'https://subsearch.vercel.app'
+        BASE_URL: 'https://findthemoment.vercel.app'
     }
 };
 const dev = {
