c064d0c0b673b787938547ec3bc06bd18e8aab78
i3p9 <i3p9@users.noreply.github.com>
2023-08-22 02:24:53 +0600

    temporarily add vercel analytics to check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom"
 import Home from './components/Home';
 import Transcript from './components/Transcript';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" Component={Home} />
         <Route path="/:selectedShow/episode/:episodeId" Component={Transcript} />
       </Routes>
+      <Analytics />
     </div>
   );
 }
